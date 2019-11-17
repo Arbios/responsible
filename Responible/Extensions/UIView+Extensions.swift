8a5e2ea580f9d6604166bdd476c037e0fe656e36
Arbi Bashaev <arbi.personal@gmail.com>
2019-11-17 03:17:54 +0300

    Added maps

---
+++ Responible/Extensions/UIView+Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  UIView+Extensions.swift
+//  Responible
+//
+//  Created by Арби Башаев on 17.11.2019.
+//  Copyright © 2019 Арби Башаев. All rights reserved.
+//
+
+import UIKit
+
+class UIView_Extensions: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
