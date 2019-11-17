b75b6dd60eedff85ad0c8824f72ae8a08c377179
Arbi Bashaev <arbi.personal@gmail.com>
2019-11-17 15:42:49 +0300

    Added moving screen and zones

---
+++ Responible/Extensions/UIButton+Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  UIButton+Extensions.swift
+//  Responible
+//
+//  Created by Арби Башаев on 17.11.2019.
+//  Copyright © 2019 Арби Башаев. All rights reserved.
+//
+
+import UIKit
+
+class UIButton_Extensions: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
